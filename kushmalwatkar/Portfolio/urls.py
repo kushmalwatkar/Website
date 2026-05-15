5959d2470353c678582916e42a8980e9073b326a
Kush Malwatkar <kushmalwatkar@gmail.com>
2026-05-15 15:25:33 -0400

    Polish home + add projects

--- kushmalwatkar/Portfolio/urls.py
+++ kushmalwatkar/Portfolio/urls.py
@@ -19,5 +19,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('projects/', views.projects, name='projects'),
+    path('experience/', views.experience, name='experience'),
+    path('contact/', views.contact, name='contact')
 ]
--- kushmalwatkar/Portfolio/views.py
+++ kushmalwatkar/Portfolio/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def projects(request):
+    return render(request, "projects.html")
+
+def experience(request):
+    return render(request, "experience.html")
+
+def contact(request):
+    return render(request, "contact.html")
